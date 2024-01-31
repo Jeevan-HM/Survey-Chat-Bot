4611a2e6dbaef677478a35430c8e0deb1fbf0812
Jeevan <jeevanhm308@gmail.com>
2024-01-31 17:41:19 +0530

    Update app

--- app.py
+++ app.py
@@ -127,6 +127,9 @@ async def main():
             key="submit_button",
             on_click=lambda: setattr(st.session_state, "submit_button_state", True),
         )
+        st.sidebar.write(
+            "Note: You can type exit any time during the conversation to exit the conversation."
+        )
 
     if st.session_state.submit_button_state:
         # Check if the bot has already been created
